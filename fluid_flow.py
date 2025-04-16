77b8f679cba5f055102288414b7217932566ebe6
tuckluck <tsanderslyle@gmail.com>
2025-04-15 20:56:16 -0400

    Add files via upload

---
+++ fluid_flow.py
@@ -0,0 +1,246 @@
+#In this problem we will explore fluid flow through a pipe. This example was taken from https://jsdokken.com/dolfinx-tutorial/chapter2/ns_code1.html
+#This example explores Fenics useage for the Navier Stokes equation
+#There will be a final visualization image created from this code that shows fluid flowing through a pipe
+
+
+
+
+from mpi4py import MPI
+from petsc4py import PETSc
+import numpy as np
+import pyvista
+
+from dolfinx.fem import Constant, Function, functionspace, assemble_scalar, dirichletbc, form, locate_dofs_geometrical
+from dolfinx.fem.petsc import assemble_matrix, assemble_vector, apply_lifting, create_vector, set_bc
+from dolfinx.io import VTXWriter
+from dolfinx.mesh import create_unit_square
+from dolfinx.plot import vtk_mesh
+from basix.ufl import element
+from ufl import (FacetNormal, Identity, TestFunction, TrialFunction,
+                 div, dot, ds, dx, inner, lhs, nabla_grad, rhs, sym)
+
+mesh = create_unit_square(MPI.COMM_WORLD, 10, 10)   #creates the unit square mesh size, which will later be visualized in the image generated
+t = 0   #states the initial time
+T = 10   #final time for flow simulation
+num_steps = 500
+dt = T / num_steps  #step size determined
+
+v_cg2 = element("Lagrange", mesh.topology.cell_name(), 2, shape=(mesh.geometry.dim, ))  
+s_cg1 = element("Lagrange", mesh.topology.cell_name(), 1)
+V = functionspace(mesh, v_cg2)     #represents scalar velocity, uses quadratic elements
+Q = functionspace(mesh, s_cg1)     #represents scalar pressure, uses linear elements
+
+
+u = TrialFunction(V)  #velocity
+v = TestFunction(V)
+p = TrialFunction(Q)  #pressure
+q = TestFunction(Q)
+
+
+#function used to locate walls for use later. Walls are important because of the no slip boundry condition in fluid flow
+#walls are defined at y=0 and y=1
+def walls(x):
+    return np.logical_or(np.isclose(x[1], 0), np.isclose(x[1], 1))
+
+
+#establishes gemetric now slip boundry for velocity
+wall_dofs = locate_dofs_geometrical(V, walls)    
+u_noslip = np.array((0,) * mesh.geometry.dim, dtype=PETSc.ScalarType)
+bc_noslip = dirichletbc(u_noslip, wall_dofs, V)
+
+#establishes function for the inflow at x=0 and t=0
+def inflow(x):
+    return np.isclose(x[0], 0)
+
+
+#establishes pressure of 8 at inflow and 0 at outflow, which drives the fluid flow
+inflow_dofs = locate_dofs_geometrical(Q, inflow)
+bc_inflow = dirichletbc(PETSc.ScalarType(8), inflow_dofs, Q)
+
+#establishes function for the outflow at x=L and t=t_final (T)
+def outflow(x):
+    return np.isclose(x[0], 1)
+
+
+outflow_dofs = locate_dofs_geometrical(Q, outflow)
+bc_outflow = dirichletbc(PETSc.ScalarType(0), outflow_dofs, Q)  #outflow pressure of zero
+bcu = [bc_noslip]   #velocity boundry conditions
+bcp = [bc_inflow, bc_outflow]   #pressure boundry conditions
+
+u_n = Function(V)  #velocity at next step
+u_n.name = "u_n"
+U = 0.5 * (u_n + u)
+n = FacetNormal(mesh)
+f = Constant(mesh, PETSc.ScalarType((0, 0)))
+k = Constant(mesh, PETSc.ScalarType(dt))
+mu = Constant(mesh, PETSc.ScalarType(1))  #viscosity defined
+rho = Constant(mesh, PETSc.ScalarType(1)) #density defined
+
+# Define strain-rate tensor
+def epsilon(u):
+    return sym(nabla_grad(u))
+
+# Define stress tensor
+
+def sigma(u, p):
+    return 2 * mu * epsilon(u) - p * Identity(len(u))
+
+
+# Define the variational problem for the first step
+p_n = Function(Q)  #pressure at next step
+p_n.name = "p_n"
+F1 = rho * dot((u - u_n) / k, v) * dx
+F1 += rho * dot(dot(u_n, nabla_grad(u_n)), v) * dx
+F1 += inner(sigma(U, p_n), epsilon(v)) * dx
+F1 += dot(p_n * n, v) * ds - dot(mu * nabla_grad(U) * n, v) * ds
+F1 -= dot(f, v) * dx
+a1 = form(lhs(F1))
+L1 = form(rhs(F1))
+
+A1 = assemble_matrix(a1, bcs=bcu)
+A1.assemble()
+b1 = create_vector(L1)
+
+# Define variational problem for step 2
+u_ = Function(V)
+a2 = form(dot(nabla_grad(p), nabla_grad(q)) * dx)
+L2 = form(dot(nabla_grad(p_n), nabla_grad(q)) * dx - (rho / k) * div(u_) * q * dx)
+A2 = assemble_matrix(a2, bcs=bcp)
+A2.assemble()
+b2 = create_vector(L2)
+
+# Define variational problem for step 3
+p_ = Function(Q)
+a3 = form(rho * dot(u, v) * dx)
+L3 = form(rho * dot(u_, v) * dx - k * dot(nabla_grad(p_ - p_n), v) * dx)
+A3 = assemble_matrix(a3)
+A3.assemble()
+b3 = create_vector(L3)
+
+# Solver for step 1
+solver1 = PETSc.KSP().create(mesh.comm)
+solver1.setOperators(A1)
+solver1.setType(PETSc.KSP.Type.BCGS)
+pc1 = solver1.getPC()
+pc1.setType(PETSc.PC.Type.HYPRE)
+pc1.setHYPREType("boomeramg")
+
+# Solver for step 2
+solver2 = PETSc.KSP().create(mesh.comm)
+solver2.setOperators(A2)
+solver2.setType(PETSc.KSP.Type.BCGS)
+pc2 = solver2.getPC()
+pc2.setType(PETSc.PC.Type.HYPRE)
+pc2.setHYPREType("boomeramg")
+
+# Solver for step 3
+solver3 = PETSc.KSP().create(mesh.comm)
+solver3.setOperators(A3)
+solver3.setType(PETSc.KSP.Type.CG)
+pc3 = solver3.getPC()
+pc3.setType(PETSc.PC.Type.SOR)
+
+from pathlib import Path
+folder = Path("results")
+folder.mkdir(exist_ok=True, parents=True)
+vtx_u = VTXWriter(mesh.comm, folder / "poiseuille_u.bp", u_n, engine="BP4")
+vtx_p = VTXWriter(mesh.comm, folder / "poiseuille_p.bp", p_n, engine="BP4")
+vtx_u.write(t)
+vtx_p.write(t)
+
+#exact velocity function is used for calculating L2 error
+def u_exact(x):
+    values = np.zeros((2, x.shape[1]), dtype=PETSc.ScalarType)
+    values[0] = 4 * x[1] * (1.0 - x[1])
+    return values
+
+
+u_ex = Function(V)
+u_ex.interpolate(u_exact)
+
+L2_error = form(dot(u_ - u_ex, u_ - u_ex) * dx)
+
+for i in range(num_steps):
+    # Update current time step
+    t += dt
+
+    # Step 1: Tentative veolcity step
+    with b1.localForm() as loc_1:
+        loc_1.set(0)
+    assemble_vector(b1, L1)
+    apply_lifting(b1, [a1], [bcu])
+    b1.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b1, bcu)
+    solver1.solve(b1, u_.x.petsc_vec)
+    u_.x.scatter_forward()
+
+    # Step 2: Pressure corrrection step
+    with b2.localForm() as loc_2:
+        loc_2.set(0)
+    assemble_vector(b2, L2)
+    apply_lifting(b2, [a2], [bcp])
+    b2.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    set_bc(b2, bcp)
+    solver2.solve(b2, p_.x.petsc_vec)
+    p_.x.scatter_forward()
+
+    # Step 3: Velocity correction step
+    with b3.localForm() as loc_3:
+        loc_3.set(0)
+    assemble_vector(b3, L3)
+    b3.ghostUpdate(addv=PETSc.InsertMode.ADD_VALUES, mode=PETSc.ScatterMode.REVERSE)
+    solver3.solve(b3, u_.x.petsc_vec)
+    u_.x.scatter_forward()
+    # Update variable with solution form this time step
+    u_n.x.array[:] = u_.x.array[:]
+    p_n.x.array[:] = p_.x.array[:]
+
+    # Write solutions to file
+    vtx_u.write(t)
+    vtx_p.write(t)
+
+    # Compute error at current time-step
+    error_L2 = np.sqrt(mesh.comm.allreduce(assemble_scalar(L2_error), op=MPI.SUM))
+    error_max = mesh.comm.allreduce(np.max(u_.x.petsc_vec.array - u_ex.x.petsc_vec.array), op=MPI.MAX)
+    # Print error only every 20th step and at the last step
+    if (i % 20 == 0) or (i == num_steps - 1):
+        print(f"Time {t:.2f}, L2-error {error_L2:.2e}, Max error {error_max:.2e}")
+# Close xmdf file
+vtx_u.close()
+vtx_p.close()
+b1.destroy()
+b2.destroy()
+b3.destroy()
+solver1.destroy()
+solver2.destroy()
+solver3.destroy()
+
+
+
+# this plotting creates a visualization of the mesh and velocity profile within the pipe
+# Force off-screen rendering
+pyvista.OFF_SCREEN = True
+
+# Build your glyphs and grid as before
+pyvista.start_xvfb()
+topology, cell_types, geometry = vtk_mesh(V)
+values = np.zeros((geometry.shape[0], 3), dtype=np.float64)
+values[:, :len(u_n)] = u_n.x.array.real.reshape((geometry.shape[0], len(u_n)))
+
+# Create a point cloud of glyphs
+function_grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
+function_grid["u"] = values
+glyphs = function_grid.glyph(orient="u", factor=0.2)
+
+# Create a pyvista-grid for the mesh
+mesh.topology.create_connectivity(mesh.topology.dim, mesh.topology.dim)
+grid = pyvista.UnstructuredGrid(*vtk_mesh(mesh, mesh.topology.dim))
+
+# Create the plotter and add the mesh and glyphs
+plotter = pyvista.Plotter(off_screen=True)
+plotter.add_mesh(grid, style="wireframe", color="k")
+plotter.add_mesh(glyphs)
+plotter.view_xy()
+
+# Save the screenshot to a file
+plotter.screenshot("pipe_flow_visual.png")
